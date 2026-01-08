7eb2c43d322792c7cc0d2dcb52c2457f67604e5c
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2026-01-08 07:53:24 +0100

    Add indexes. Remove fnr in response for now

--- src/main/kotlin/no/nav/syfo/document/api/v1/dto/DialogResponse.kt
+++ src/main/kotlin/no/nav/syfo/document/api/v1/dto/DialogResponse.kt
@@ -1,12 +1,11 @@
 package no.nav.syfo.document.api.v1.dto
 
 import java.time.Instant
-import java.util.*
+import java.util.UUID
 
 data class DialogResponse(
     val title: String,
     val summary: String?,
-    val fnr: String,
     val orgNumber: String,
     val dialogportenUUID: UUID? = null,
     val created: Instant,
--- src/main/kotlin/no/nav/syfo/document/db/DialogEntity.kt
+++ src/main/kotlin/no/nav/syfo/document/db/DialogEntity.kt
@@ -1,8 +1,8 @@
 package no.nav.syfo.document.db
 
+import no.nav.syfo.document.api.v1.dto.DialogResponse
 import java.time.Instant
 import java.util.UUID
-import no.nav.syfo.document.api.v1.dto.DialogResponse
 
 open class DialogEntity(
     open val title: String,
@@ -32,7 +32,6 @@ data class PersistedDialogEntity(
         DialogResponse(
             title = title,
             summary = summary,
-            fnr = fnr,
             orgNumber = orgNumber,
             dialogportenUUID = dialogportenUUID,
             created = created,
