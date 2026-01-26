04266dc4a17b6e15f1aea1d50b85eeccd1433c0d
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-01-26 08:34:31 +0100

    Upgrade Engine in HttpClient to Apache5

--- src/main/kotlin/no/nav/syfo/util/HttpClient.kt
+++ src/main/kotlin/no/nav/syfo/util/HttpClient.kt
@@ -5,13 +5,13 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
+import io.ktor.client.engine.apache5.Apache5
 import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.jackson.jackson
 
-fun httpClientDefault(httpClient: HttpClient = HttpClient(Apache)): HttpClient = httpClient.config {
+fun httpClientDefault(httpClient: HttpClient = HttpClient(Apache5)): HttpClient = httpClient.config {
     expectSuccess = true
     install(ContentNegotiation) {
         jackson {
