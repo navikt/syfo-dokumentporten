d882468f2de2560b80e09977d4a7c4f0a4bff64d
malazkoj <malaz.alkoj@nav.no>
2026-03-11 16:28:44 +0100

    Remove double code.

--- src/test/kotlin/no/nav/syfo/document/api/v1/dto/DocumentTest.kt
+++ src/test/kotlin/no/nav/syfo/document/api/v1/dto/DocumentTest.kt
@@ -74,24 +74,6 @@ class DocumentTest :
                 Innholdet er tilgjengelig i 4 måneder fra delingsdatoen. 
                 """.trimIndent()
             }
-
-            it("should create correct title and summary from d-nummer when fullName is null") {
-                // Arrange
-                val document = document().copy(
-                    fullName = null,
-                    fnr = "41011999000", // d-nummer
-                    birthDate = null, // No birthDate
-                )
-                // Act
-                val dialog = document.toDialogEntity()
-                // Assert
-                dialog.title shouldBe "Sykefraværsoppfølging for 41011999000"
-                dialog.summary shouldBe """
-                Her finner du alle dialogmøtebrev fra Nav og oppfølgingsplaner utarbeidet av nærmeste leder for 41011999000.
-                Innholdet er tilgjengelig i 4 måneder fra delingsdatoen. 
-                """.trimIndent()
-            }
-
             it("should set correct orgNumber and fnr") {
                 // Arrange
                 val document = document()
