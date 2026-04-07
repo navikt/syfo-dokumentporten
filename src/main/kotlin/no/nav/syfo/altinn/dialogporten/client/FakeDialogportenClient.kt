31ae05c647ea634dfad2095d51999a909d3de646
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-04-07 09:43:47 +0200

    Remove deleteDialog from DialogportenClient

--- src/main/kotlin/no/nav/syfo/altinn/dialogporten/client/FakeDialogportenClient.kt
+++ src/main/kotlin/no/nav/syfo/altinn/dialogporten/client/FakeDialogportenClient.kt
@@ -1,6 +1,5 @@
 package no.nav.syfo.altinn.dialogporten.client
 
-import io.ktor.http.HttpStatusCode
 import no.nav.syfo.altinn.dialogporten.domain.Dialog
 import no.nav.syfo.altinn.dialogporten.domain.Transmission
 import no.nav.syfo.util.logger
@@ -14,13 +13,4 @@ class FakeDialogportenClient : IDialogportenClient {
     override suspend fun createDialog(dialog: Dialog): UUID = UUID.randomUUID()
 
     override suspend fun addTransmission(transmission: Transmission, dialogId: UUID): UUID = UUID.randomUUID()
-
-    override suspend fun deleteDialog(dialogId: UUID): HttpStatusCode {
-        logger.info("Deleting dialog with id: $dialogId")
-        return if (dialogId == UUID.fromString("5411e06d-36e7-46b2-984f-6c6405e57160")) {
-            HttpStatusCode.NotFound
-        } else {
-            HttpStatusCode.NoContent
-        }
-    }
 }
