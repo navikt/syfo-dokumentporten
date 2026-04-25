2c2e4dedf5b719368ecbac3eebe0cdd19c249547
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-04-25 17:54:44 +0200

    Add deserialization of all properties from authorize response, and log the response on failure.

--- src/main/kotlin/no/nav/syfo/altinn/pdp/client/PdpResponse.kt
+++ src/main/kotlin/no/nav/syfo/altinn/pdp/client/PdpResponse.kt
@@ -1,8 +1,18 @@
 package no.nav.syfo.altinn.pdp.client
 
-data class PdpResponse(val response: List<DecisionResult>,)
+import no.nav.syfo.util.logger
 
-data class DecisionResult(val decision: Decision,)
+val logger = logger("PDPResponse")
+data class PdpResponse(val response: List<DecisionResult>)
+
+data class DecisionResult(
+    val decision: Decision,
+    val status: PdpStatus? = null,
+    val obligations: List<PdpObligation>? = null,
+    val associateAdvice: List<PdpAdvice>? = null,
+    val category: List<PdpCategory>? = null,
+    val policyIdentifierList: PolicyIdentifierList? = null,
+)
 
 enum class Decision {
     Permit,
@@ -11,6 +21,55 @@ enum class Decision {
     Deny,
 }
 
-fun PdpResponse.resultat() = response.first().decision
+data class PdpStatus(
+    val statusMessage: String? = null,
+    val statusDetails: List<String>? = null,
+    val statusCode: PdpStatusCode? = null,
+)
 
-fun PdpResponse.harTilgang(): Boolean = resultat() == Decision.Permit
+data class PdpStatusCode(val value: String? = null, val statusCode: String? = null)
+
+data class PdpObligation(val id: String? = null, val attributeAssignment: List<AttributeAssignment>? = null)
+
+data class PdpAdvice(val id: String? = null, val attributeAssignment: List<AttributeAssignment>? = null)
+
+data class AttributeAssignment(
+    val attributeId: String? = null,
+    val value: String? = null,
+    val category: String? = null,
+    val dataType: String? = null,
+    val issuer: String? = null,
+)
+
+data class PdpCategory(
+    val categoryId: String? = null,
+    val id: String? = null,
+    val content: String? = null,
+    val attribute: List<PdpAttribute>? = null,
+)
+
+data class PdpAttribute(
+    val attributeId: String? = null,
+    val value: String? = null,
+    val issuer: String? = null,
+    val dataType: String? = null,
+    val includeInResult: Boolean? = null,
+)
+
+data class PolicyIdentifierList(
+    val policyIdReference: List<PolicyReference>? = null,
+    val policySetIdReference: List<PolicyReference>? = null,
+)
+
+data class PolicyReference(val id: String? = null, val version: String? = null)
+
+// fun PdpResponse.resultat() = response.first().decision
+
+fun PdpResponse.harTilgang(): Boolean {
+    val failed = response.filterNot { it.decision == Decision.Permit }
+    if (failed.isNotEmpty()) {
+        logger.info("PDP response har ikke tilgang: response=$response")
+        return false
+    }
+    return true
+}
