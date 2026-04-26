6d4aa2e7ff8dad778e4ee99042591897f34aebe9
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-04-26 20:31:45 +0200

    Log orgnumbers we will check

--- src/main/kotlin/no/nav/syfo/altinn/pdp/service/PdpService.kt
+++ src/main/kotlin/no/nav/syfo/altinn/pdp/service/PdpService.kt
@@ -3,10 +3,13 @@ package no.nav.syfo.altinn.pdp.service
 import no.nav.syfo.altinn.pdp.client.Bruker
 import no.nav.syfo.altinn.pdp.client.IPdpClient
 import no.nav.syfo.altinn.pdp.client.harTilgang
+import no.nav.syfo.util.logger
 
 class PdpService(private val pdpClient: IPdpClient,) {
 
+    val logger = logger()
     suspend fun hasAccessToResource(bruker: Bruker, orgnrSet: Set<String>, ressurs: String): Boolean {
+        logger.info("PDP access check for orgnumer $orgnrSet for ressurs $ressurs")
         val pdpResponse = pdpClient.authorize(bruker, orgnrSet, ressurs)
         return pdpResponse.harTilgang()
     }
